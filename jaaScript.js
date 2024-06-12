3965e871dc8a4ded53d9108cd60b358e5f8966bc
vkatari10 <kaskat2021@gmail.com>
2024-06-12 12:02:25 -0400

    created new apple game

---
+++ jaaScript.js
@@ -0,0 +1,18 @@
+//DOM
+
+
+//variables
+let apples = 0;
+let workers = 2;
+let dailyCollection = 0;
+let dailyLoss = 0;
+let backpackApples = 0;
+let trashbagApples = 0;
+let trashbags = 0;
+let forestBags = 0;
+let shedBags = 0;
+let appleBombers = 0;
+let appleSoldiers = 2;
+let minApples = 0;
+let successChance = 0;
+const houseToApple = ["Wilkin House", "AirTime Thrills", "Random House Party", "Wiki HQ"];
